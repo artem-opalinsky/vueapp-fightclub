960c2a658913b73989d66ad8344dcdcec9f1b342
artem-opalinsky <artem.opalinsky@mail.ru>
2020-09-03 16:56:17 +0300

    First commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item>
+        <router-link to="/">Fight Club</router-link>
+      </b-nav-item>
+      <b-nav-item>
+        <router-link to="/">Posts</router-link>
+      </b-nav-item>
+
+
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+  export default {
+
+  }
+
+</script>
---
+++ src/views/Posts.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="posts">
+    <Navbar />
+    <div v-for="posts in APIData" :key="posts.id">
+      <b-container>
+        <b-row>
+          {{posts.title}}
+        </b-row>
+        <b-row>
+          {{posts.content}}
+        </b-row>
+      </b-container>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from "@/components/Navbar"
+  import { getAPI } from '../axios.api'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+  export default {
+    name: 'Posts',
+    data() {
+      return{
+        APIData: []
+      }
+    },
+    components:{
+      Navbar
+    },
+    created() {
+      getAPI.get('/posts/',)
+        .then(response => {
+          console.log('Post API has recieved data')
+          this.APIData = response.data
+        })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
